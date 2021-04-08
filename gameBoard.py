51b86b00a1ec0e92b480fc47f2c6861aad6b8a96
JustinTravisCoffey <43015255+JustinTravisCoffey@users.noreply.github.com>
2021-04-08 12:03:44 -0700

    Update gameBoard.py
    Part of my changes to get the display working

--- gameBoard.py
+++ gameBoard.py
@@ -4,18 +4,34 @@ import tkinter as tk
 
 class Board(tk.Tk):
 
-    def __init__(self, init_board, gamemessage):
-        title = tk.label(text="Helma")
-        for row in init_board:
-            for elem in row:
-                if elem[2] == 2:
-                    button = tk.Button(bg='green')
-                    button.grid(row=elem[0], column=elem[1], pady=2)
-                elif elem[2] == 1:
-                    button = tk.Button(bg='red')
-                    button.grid(row=elem[0], column=elem[1], pady=2)
-                else:
-                    button = tk.Button(bg='white')
-                    button.grid(row=elem[0], column=elem[1], pady=2)
+    def __init__(self, b_size, gamemessage):
 
-        messagedisplay = tk.label(text=gamemessage)
+        # Initialize parent tk class
+        tk.Tk.__init__(self)
+        self.buttonframe = tk.Frame(self)
+        self.buttonframe.grid(row=0, column=0, pady=2, padx=2, sticky="NSEW")
+        self.messageframe = tk.Frame(self)
+        self.messageframe.grid(row=1, column=0, pady=2, padx=2, sticky="NSEW")
+
+
+        # title text
+        self.wm_title("Halma!!!!")
+        self.resizable(False, False)
+
+        for row in range(b_size):
+            for col in range(b_size):
+
+                if row + col < 4:
+                    self.button = tk.Button(self.buttonframe, bg='red', width=4, height=2)
+                    self.button.grid(row=int(row), column=int(col), pady=2, padx=2, sticky="NSEW")
+
+
+                elif row + col > 2 * (b_size - 3):
+                    self.button = tk.Button(self.buttonframe, bg='green', width=4, height=2)
+                    self.button.grid(row=int(row), column=int(col), pady=2, padx=2, sticky="NSEW")
+
+                else:
+                    self.button = tk.Button(self.buttonframe, bg='white', width=4, height=2)
+                    self.button.grid(row=int(row), column=int(col), pady=2, padx=2, sticky="NSEW")
+            self.displaymessage = tk.Label(self.messageframe, text=gamemessage, anchor="nw")
+            self.displaymessage.grid(row=b_size+1, column=0, pady=2, padx=2, )
